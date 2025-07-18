1b2a7ab7ac8daf2c14c87952739c241a6477f2f9
Tomcat <polpware@gmail.com>
2025-07-18 07:20:19 -0400

    New publish

--- lib/components/modals/observable-modal-abstract.component.d.ts
+++ lib/components/modals/observable-modal-abstract.component.d.ts
@@ -12,6 +12,6 @@ export declare abstract class ObservableModalAbstractComponent<TInput, TOutput> 
     protected abstract bsModalService: BsModalService;
     closeModal(value: TOutput): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<ObservableModalAbstractComponent<any, any>, never>;
-    static ɵdir: i0.ɵɵDirectiveDeclaration<ObservableModalAbstractComponent<any, any>, never, never, { "modalId": { "alias": "modalId"; "required": false; }; }, {}, never, never, false, never>;
+    static ɵdir: i0.ɵɵDirectiveDeclaration<ObservableModalAbstractComponent<any, any>, never, never, { "modalId": { "alias": "modalId"; "required": false; }; }, {}, never, never, true, never>;
 }
 //# sourceMappingURL=observable-modal-abstract.component.d.ts.map
